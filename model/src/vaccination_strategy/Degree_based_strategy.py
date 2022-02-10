84925cf63ae4acf45eba1665a647ebb16decf285
Rohan Kamat <rohankamat20022002@gmail.com>
2022-02-11 00:02:28 +0530

    Added distinct degree based strategy and simulation

--- model/src/vaccination_strategy/Degree_based_strategy.py
+++ model/src/vaccination_strategy/Degree_based_strategy.py
@@ -1,4 +1,34 @@
 import pandas as pd
+'''import math
+adj = []
+class node:
+    def __init__(self,id,left = None,right = None):
+        self.id = id
+        self.left = left
+        self.right = right
+        self.right_length = 0
+        self.left_length = 0
+    def search(self,person):
+        if not (self):
+            return 0
+        if person == self.id:
+            return 1
+        elif person > self.id:
+            search(self.right,person)
+        else:
+            search(self.left,person)
+    def Insert(self,p):
+        if self.right_length == self.left_length:
+            if p.id > self.id:
+                if self.right == None:
+                    self.right = p
+                else:
+                    Insert(self.right,n)
+                self.right_length += 1
+            else: 
+                Insert(self.left,n)
+                self.left_length += 1
+        elif self.right_'''
 
 def vaccination_strategy(model,vac_percent,D_day):
     size = model.df.shape[0]
@@ -13,8 +43,12 @@ def vaccination_strategy(model,vac_percent,D_day):
     per_day = Range // days
 
     degree = []
+    adj = []
+
     for i in range(pop_count):
         degree.append(0)
+        #adj.append(None)
+        adj.append([])
 
     prev_time = int(model.df["Time"][1])
     time = prev_time
@@ -30,8 +64,17 @@ def vaccination_strategy(model,vac_percent,D_day):
 
         n1 = int(model.df["Person 1"][time])
         n2 = int(model.df["Person 2"][time])
-        degree[n1 - start_index] += 1
-        degree[n2 - start_index] += 1
+        #if not(adj[n1 - start_index]) or not(adj[n1 - start_index].search(n2)):
+        if n2 not in adj[n1 - start_index]:
+            degree[n1 - start_index] += 1
+            degree[n2 - start_index] += 1
+            #person1 = node(n1)
+            #person2 = node(n2)
+            #adj[n1 - start_index].Insert(person2)
+            #adj[n2 - start_index].Insert(person1)
+            adj[n1 - start_index].append(n2)
+            adj[n2 - start_index].append(n1)
+
         time += 1
 
     no_vaccinated = int(pop_count * (vac_percent / 100))
