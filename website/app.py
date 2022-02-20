4651317a73107b53ba4c6680d7e67ea475f9f169
SureshKamediya <suresh.191cs158@nitk.edu.in>
2022-02-20 15:36:53 +0530

    Added code to display plot

--- website/app.py
+++ website/app.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import os
+import io
 import random
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect, Response
 from werkzeug.utils import secure_filename
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import numpy as np
 
 
 from utils.utils import SIR, run
@@ -11,6 +15,12 @@ from utils.random_strategy import vaccination_strategy
 
 
 app = Flask(__name__)
+
+class DataStore():
+    result = None
+
+data = DataStore()
+
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == "POST":
@@ -22,23 +32,50 @@ def index():
         metadata.save(filepath1)
         filepath2 = os.path.join('uploads', filename2)
         df.save(filepath2)
-        return redirect(url_for('prediction', filepath1=filepath1, filepath2= filepath2))
+
+        metadata = pd.read_csv(filepath1,sep='\t', lineterminator='\n', names=['ID', 'Class', 'Sex'])
+        df = pd.read_csv(filepath2)
+        model = SIR(
+            metadata=metadata,
+            df=df
+        )
+        vaccinated = vaccination_strategy(model)
+        result = run(model,vaccinated,5)
+        data.result = result
+        return redirect(url_for('prediction', result = result))
     return render_template('index.html')
 
-@app.route('/prediction')
+@app.route('/prediction', methods = ['POST', 'GET'])
 def prediction():
-    filepath1 = request.args['filepath1']
-    filepath2 = request.args['filepath2']
-    metadata = pd.read_csv(filepath1,sep='\t', lineterminator='\n', names=['ID', 'Class', 'Sex'])
-    df = pd.read_csv(filepath2)
-    model = SIR(
-    metadata=metadata,
-    df=df
-    )
-    vaccinated = vaccination_strategy(model)
-    output = run(model,vaccinated,5)
-    return render_template('predict.html', output = output)
+    result = request.args['result']
+    return render_template('predict.html', result = result)
 
+
+@app.route('/plot.png')
+def plot_png():
+    output = data.result
+    no_sus = np.array(output['stats']['susceptible'])
+    no_inf = np.array(output['stats']['infected'])
+    no_rec = np.array(output['stats']['recovered'])
+    no_dec = np.array(output['stats']['deceased'])
+    time = np.array(range(len(no_sus)))
+
+    fig, axis = plt.subplots(2, 2)
+    axis[0,0].plot(time, no_sus)
+    axis[0,0].set_title("Suspected")
+    
+    axis[0,1].plot(time, no_inf)
+    axis[0,1].set_title("Infected")
+
+    axis[1,0].plot(time, no_rec)
+    axis[1,0].set_title("Recovered")
+
+    axis[1,1].plot(time, no_sus)
+    axis[1,1].set_title("Deceased")
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype = 'image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
